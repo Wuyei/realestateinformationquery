fc99bca293f4dbabcd8eb8a1bde35c881eba57ec
Wuyei <wuzj5@126.com>
2018-11-15 09:47:35 +0800

    加jar包与建包结构

---
+++ realestateinformationquery/src/test/java/realestateinformationquery/Test1.java
@@ -0,0 +1,17 @@
+package realestateinformationquery;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test1 {
+
+    @Test
+    public void test() {
+        Integer i = 3;
+        String s = "df";
+        String str = "df";
+        System.out.println(s instanceof String);
+    }
+
+}
